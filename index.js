2f0251449d1ef0c75d804d18d6289d06a5d1e1c0
rozer4heros <rozer4heros@gmail.com>
2026-07-02 15:15:48 +0900

    refactor: replace mysql with mysql2 and implement database connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
 
@@ -9,10 +10,27 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mynameisytg4$",
+  database: "bbs",
+});
+
+db.connect();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 // "/"(루트)로 요청이 get으로 들어오면 res.send()의 인자를 반환
+
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT * FROM board";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
