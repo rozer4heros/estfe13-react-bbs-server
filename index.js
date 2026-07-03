c0afeb7e8fe824149f9fe40d6e927766d5096281
rozer4heros <rozer4heros@gmail.com>
2026-07-03 15:14:11 +0900

    feat: 게시물 수정 request 받기 추가

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   // "/"(루트)로 요청이 get으로 들어오면 res.send()의 인자를 반환
 });
 
-// 홈페이지
+// 게시물 목록 (홈페이지)
 app.get("/list", (req, res) => {
   const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board";
   db.query(sqlQuery, (err, result) => {
@@ -41,9 +41,9 @@ app.get("/list", (req, res) => {
   });
 });
 
-// 글 보기 페이지
+// 게시물 보기
 app.get("/view", (req, res) => {
-  console.log(req.query.id);
+  console.log(`View request: ${req.query.id}`);
   const { id } = req.query;
   // const sqlQuery = `SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=${id}`;
   const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?";
@@ -53,14 +53,25 @@ app.get("/view", (req, res) => {
   });
 });
 
-// 글 쓰기 페이지
+// 게시물 작성
 app.post("/write", (req, res) => {
-  console.log(req.body);
+  console.log(`Write request: ${req.body}`);
   const { title, content, name } = req.body;
   /* 보안에 취약함! */
   // const sqlQuery = `INSERT INTO board (title, content, writer) values(${title},${content},${name});`;
   const sqlQuery = "INSERT INTO board (title, content, writer) values(?,?,?);";
   db.query(sqlQuery, [title, content, name], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// 게시물 수정
+app.post("/update", (req, res) => {
+  console.log(`Update request: ${req.body}`);
+  const { title, content, name, id } = req.body;
+  const sqlQuery = "UPDATE board SET title=?, content=?, writer=? WHERE id=?;";
+  db.query(sqlQuery, [title, content, name, id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
